f2cebed4c2b8daa7d08ba7188dd9eee8493c9050
wuyazishidi <2604591896@qq.com>
2022-06-09 23:57:49 +0800

    PEListener

--- Assets/PEListener/Scrips/PEListener.cs
+++ Assets/PEListener/Scrips/PEListener.cs
@@ -1,36 +1,44 @@
+﻿/*************************************************
+	作者: Plane
+	邮箱: 1785275942@qq.com
+	日期: 2021/03/07 3:34
+	功能: UI事件监听插件
+
+    //=================*=================\\
+           教学官网：www.qiqiker.com
+           关注微信服务号: qiqikertuts
+           关注微信公众号: PlaneZhong
+               ~~获取更多教学资讯~~
+    \\=================*=================//
+*************************************************/
+
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PEListener : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler {
-    public Action<object> onClick;
-    public Action<PointerEventData> onClickDown;
-    public Action<PointerEventData> onClickUp;
-    public Action<PointerEventData> onDrag;
+public class PEListener :
+    MonoBehaviour,
+    IPointerClickHandler,
+    IPointerDownHandler,
+    IPointerUpHandler,
+    IDragHandler {
+    public Action<PointerEventData, object[]> onClick;
+    public Action<PointerEventData, object[]> onClickDown;
+    public Action<PointerEventData, object[]> onClickUp;
+    public Action<PointerEventData, object[]> onDrag;
 
-    public object args;
+    public object[] args = null;
 
     public void OnPointerClick(PointerEventData eventData) {
-        if (onClick != null) {
-            onClick(args);
-        }
+        onClick?.Invoke(eventData, args);
     }
-
-
     public void OnPointerDown(PointerEventData eventData) {
-        if (onClickDown != null) {
-            onClickDown(eventData);
-        }
+        onClickDown?.Invoke(eventData, args);
     }
     public void OnPointerUp(PointerEventData eventData) {
-        if (onClickUp != null) {
-            onClickUp(eventData);
-        }
+        onClickUp?.Invoke(eventData, args);
     }
     public void OnDrag(PointerEventData eventData) {
-        if (onDrag != null) {
-            onDrag(eventData);
-        }
+        onDrag?.Invoke(eventData, args);
     }
-
 }
